cb7da9f4730e69204a0daf8c9d108d74a6ef988f
MrPerry365 <1987nowhere@gmail.com>
2022-11-18 11:55:45 -0500

    initial commit

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+  return (
+    <section className="m-5">
+      <h1 id="about"> Who am I?</h1>
+      <img src={coverImage} className="m-2" style={{width: "100%"}} alt="cover"/>
+    </section>
+  
+  );
+}
+
+export default About;
